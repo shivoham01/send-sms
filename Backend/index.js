4167963c655f11eea6fd57b660356012f1164f87
shivoham01 <theashutoshshukl@gmail.com>
2024-12-30 11:22:32 +0530

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const twilio = require('twilio');
+const cors = require('cors');
+
+require('dotenv').config();
+
+// Twilio credentials
+const accountSid = process.env.accountSid;
+const authToken = process.env.authToken;
+const twilioPhoneNumber = process.env.twilioPhoneNumber;
+
+const client = twilio(accountSid, authToken);
+
+const app = express();
+app.use(bodyParser.json());
+
+// Use the CORS middleware
+app.use(cors());
+
+app.post('/send-sms', (req, res) => {
+  const { phoneNumber, message } = req.body;
+
+  client.messages
+    .create({
+      body: message,
+      from: twilioPhoneNumber,
+      to: phoneNumber,
+    })
+    .then((message) => {
+      res.json({ success: true, message: 'SMS sent successfully!' });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json({ success: false, message: 'Failed to send SMS.' });
+    });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
